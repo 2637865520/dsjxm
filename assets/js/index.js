b1aa0f5decb26872805349074ddd23cb6c822ac5
zsjingyu <2637865520@qq.com>
2020-12-31 10:09:23 +0800

    个人中心全部完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,42 @@
-$(function(){
+$(function () {
     getUserInfo();
     let layer = layui.layer;
-    $('#btnOut').on('click',function(){
-        layer.confirm('确定要退出登录?', {icon: 3, title:'提示'}, function(index){
+    $('#btnOut').on('click', function () {
+        layer.confirm('确定要退出登录?', { icon: 3, title: '提示' }, function (index) {
             //do something
             localStorage.removeItem('token');
             location.href = '/login.html';
-            
+
             layer.close(index);
-          });
-          
-                
+        });
+
+
     })
 })
-function getUserInfo(){
+function getUserInfo() {
     $.ajax({
-        method:'GET',
-        url:'/my/userinfo',
-       
-        success:function(res){
-            if(res.status !== 0){
+        method: 'GET',
+        url: '/my/userinfo',
+
+        success: function (res) {
+            if (res.status !== 0) {
                 return layui.layer.msg('获取用户信息失败!');
             }
             rendAvatar(res.data);
         },
-     /*    complete:function(res){
-      
-        } */
-        
+        /*    complete:function(res){
+         
+           } */
+
     })
 }
-function rendAvatar(user){
+function rendAvatar(user) {
     let name = user.nickname || user.username;
     $('#wlcome').html('欢迎&nbsp;&nbsp;' + name);
-    if(user.user_pic !== null){
-        $('.layui-nav-img').attr('url',user.user_pic).show();
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
         $('.text-avatar').hide();
-    }else{
+    } else {
         $('.layui-nav-img').hide();
         let first = name[0].toUpperCase();
         $('.text-avatar').html(first).show();
